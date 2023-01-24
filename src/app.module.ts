1c94dea76b4137d76301175ad2d7e553ac6024b0
Ali Haydar Sevgili <haydar.sevgili@ideabilisim.com>
2023-01-24 18:15:24 +0300

    configModule added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,33 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type:'sqlite',
-    database:'db.sqlite',
-    synchronize:true,
-    entities:[User,Report]
-  }),UsersModule, ReportsModule],
-
+  imports: [
+    UsersModule,
+    ReportsModule,
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+      ignoreEnvVars: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   synchronize: true,
+    //   entities: [User, Report],
+    // }),
+  ],
 })
 export class AppModule {}
